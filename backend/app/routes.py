d0a229817ab7efe08dab15909ec23722e5b93e41
dk2la <sjakku@student.21-school.ru>
2021-12-05 19:01:47 +0300

    create login form by guide

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,8 +1,23 @@
+# -*- coding: utf-8 -*- 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
-
+    user = {'username': 'aboba'}
+    posts = [
+        {
+            'author': {'username': 'aboba1'},
+            'body': {'GG'}
+        },
+        {
+            'author': {'username': 'aboba2'},
+            'body': {'GL'}
+        },
+        {
+            'author': {'username': 'aboba3'},
+            'body': {'HF'}
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
